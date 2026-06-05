ac68cb946a00697edcb3585ed7a3992113f7a50a
younes <younesabourrig@gmail.com>
2026-06-05 21:19:30 +0100

    fix the missmatching between Spatie and hardcoded role column in db x 2 hahaha

--- backend/database/migrations/2026_05_03_202310_add_fildes_to_users_table.php
+++ backend/database/migrations/2026_05_03_202310_add_fildes_to_users_table.php
@@ -22,7 +22,7 @@ return new class extends Migration {
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn(['adress', 'image', 'role']);
+            $table->dropColumn(['adress', 'image']);
         });
     }
 };
